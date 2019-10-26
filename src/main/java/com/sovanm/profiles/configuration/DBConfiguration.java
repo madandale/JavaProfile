0c151ac8152e543ebced2a52c208588599e1a4c9
Madan Dale <madan.dale@kpit.com>
2019-10-26 19:21:31 +0530

    initial commit

---
+++ src/main/java/com/sovanm/profiles/configuration/DBConfiguration.java
@@ -0,0 +1,76 @@
+package com.sovanm.profiles.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+public class DBConfiguration {
+	
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Profile("dev")
+	@Bean
+	public String devDatabaseConnection() {
+		System.out.println("DB connection for DEV - environment");
+		System.out.println(driverClassName);
+		System.out.println(url);
+		return "DB connection for DEV - H2";
+	}
+
+	@Profile("uat")
+	@Bean
+	public String testDatabaseConnection() {
+		System.out.println("DB Connection to UAT - Low Cost Instance");
+		System.out.println(driverClassName);
+		System.out.println(url);
+		return "DB Connection to UAT - Low Cost Instance";
+	}
+
+	@Profile("prod")
+	@Bean
+	public String prodDatabaseConnection() {
+		System.out.println("DB Connection to RDS_PROD - High Performance Instance");
+		System.out.println(driverClassName);
+		System.out.println(url);
+		return "DB Connection to RDS_PROD - High Performance Instance";
+	} 
+}
